c779aecd8eca5704df2b34e68bb5219b0bc32303
diogoaasousa23 <diogoaadesousa@gmail.com>
2025-01-18 21:50:30 +0000

    m

--- Armazem.java
+++ Armazem.java
@@ -1,24 +1,43 @@
 import java.util.ArrayList;
+
 public class Armazem {
     private String nome;
+    private String morada;  // Novo atributo morada
     private double capacidadeEspaco;
     private ArrayList<Mercadoria> mercadorias = new ArrayList<>();
-
     private boolean reciclagem;  // Indica se o armazém é de reciclagem
-    
+
     // Modificação do construtor
     public Armazem(String nome, String morada, double capacidadeEspaco, boolean reciclagem) {
         this.nome = nome;
+        this.morada = morada;  // Inicializando o campo morada
         this.capacidadeEspaco = capacidadeEspaco;
         this.mercadorias = new ArrayList<>();
         this.reciclagem = reciclagem;
     }
+
+    // Métodos getters
     public String getNome() {
         return nome;
     }
+
+    public String getMorada() {
+        return morada;  // Getter para o campo morada
+    }
+
     public boolean isReciclagem() {
         return reciclagem;
     }
+
+    public double getCapacidadeEspaco() {
+        return capacidadeEspaco;
+    }
+
+    public ArrayList<Mercadoria> getMercadorias() {
+        return mercadorias;
+    }
+
+    // Método para adicionar mercadoria ao armazém
     public boolean adicionarMercadoria(Mercadoria mercadoria) {
         if (capacidadeEspaco >= mercadoria.getVolume()) {
             mercadorias.add(mercadoria);
@@ -32,16 +51,7 @@ public class Armazem {
         }
     }
 
-    public double getCapacidadeEspaco() {
-        return capacidadeEspaco;
-    }
-    
-    public ArrayList<Mercadoria> getMercadorias() {
-        return mercadorias;
-    }
-    
-    
-    
+    // Método para remover mercadoria do armazém
     public boolean removerMercadoria(Mercadoria mercadoria) {
         if (reciclagem) {
             // Armazéns de reciclagem não podem remover mercadorias
--- Persistencia.java
+++ Persistencia.java
@@ -3,37 +3,81 @@ import java.util.ArrayList;
 
 public class Persistencia {
 
-    // Salvar os armazéns, mercadorias e transportadoras
+    // Método para salvar os dados
     public static void salvarDados(ArrayList<Armazem> armazens, ArrayList<Mercadoria> mercadorias, ArrayList<Transportadora> transportadoras) {
-        try (ObjectOutputStream armazemOut = new ObjectOutputStream(new FileOutputStream("armazens.dat"));
-             ObjectOutputStream mercadoriaOut = new ObjectOutputStream(new FileOutputStream("mercadorias.dat"));
-             ObjectOutputStream transportadoraOut = new ObjectOutputStream(new FileOutputStream("transportadoras.dat"))) {
-            
-            armazemOut.writeObject(armazens);
-            mercadoriaOut.writeObject(mercadorias);
-            transportadoraOut.writeObject(transportadoras);
-            
+        try {
+            // Salvar armazéns
+            BufferedWriter writerArmazens = new BufferedWriter(new FileWriter("armazens.txt"));
+            for (Armazem armazem : armazens) {
+                writerArmazens.write(armazem.getNome() + "," + armazem.getMorada() + "," + armazem.getCapacidadeEspaco() + "," + armazem.isReciclagem() + "\n");
+            }
+            writerArmazens.close();
+
+            // Salvar mercadorias
+            BufferedWriter writerMercadorias = new BufferedWriter(new FileWriter("mercadorias.txt"));
+            for (Mercadoria mercadoria : mercadorias) {
+                writerMercadorias.write(mercadoria.getId() + "," + mercadoria.getDescricao() + "," + mercadoria.getTipo() + "," + mercadoria.getPeso() + "," + mercadoria.getVolume() + "," + mercadoria.getTagIoT() + "\n");
+            }
+            writerMercadorias.close();
+
+            // Salvar transportadoras
+            BufferedWriter writerTransportadoras = new BufferedWriter(new FileWriter("transportadoras.txt"));
+            for (Transportadora transportadora : transportadoras) {
+                writerTransportadoras.write(transportadora.getId() + "," + transportadora.getNome() + "," + transportadora.getCapacidadePeso() + "," + transportadora.getCapacidadeVolume() + "," + transportadora.isEspecial() + "\n");
+            }
+            writerTransportadoras.close();
+
             System.out.println("Dados salvos com sucesso!");
         } catch (IOException e) {
             System.out.println("Erro ao salvar os dados: " + e.getMessage());
         }
     }
 
-    // Carregar os armazéns, mercadorias e transportadoras
-    @SuppressWarnings("unchecked")
+    // Método para carregar os dados
     public static void carregarDados(ArrayList<Armazem> armazens, ArrayList<Mercadoria> mercadorias, ArrayList<Transportadora> transportadoras) {
-        try (ObjectInputStream armazemIn = new ObjectInputStream(new FileInputStream("armazens.dat"));
-             ObjectInputStream mercadoriaIn = new ObjectInputStream(new FileInputStream("mercadorias.dat"));
-             ObjectInputStream transportadoraIn = new ObjectInputStream(new FileInputStream("transportadoras.dat"))) {
+        try {
+            // Carregar armazéns
+            BufferedReader readerArmazens = new BufferedReader(new FileReader("armazens.txt"));
+            String linha;
+            while ((linha = readerArmazens.readLine()) != null) {
+                String[] dados = linha.split(",");
+                String nome = dados[0];
+                String morada = dados[1];
+                double capacidadeEspaco = Double.parseDouble(dados[2]);
+                boolean reciclagem = Boolean.parseBoolean(dados[3]);
+                armazens.add(new Armazem(nome, morada, capacidadeEspaco, reciclagem));
+            }
+            readerArmazens.close();
 
-            ArrayList<Armazem> armazemList = (ArrayList<Armazem>) armazemIn.readObject();
-            armazens.addAll(armazemList);
-            armazens.addAll((ArrayList<Armazem>) armazemIn.readObject());
-            mercadorias.addAll((ArrayList<Mercadoria>) mercadoriaIn.readObject());
-            transportadoras.addAll((ArrayList<Transportadora>) transportadoraIn.readObject());
-            
+            // Carregar mercadorias
+            BufferedReader readerMercadorias = new BufferedReader(new FileReader("mercadorias.txt"));
+            while ((linha = readerMercadorias.readLine()) != null) {
+                String[] dados = linha.split(",");
+                int id = Integer.parseInt(dados[0]);
+                String descricao = dados[1];
+                String tipo = dados[2];
+                double peso = Double.parseDouble(dados[3]);
+                double volume = Double.parseDouble(dados[4]);
+                String tagIoT = dados[5];
+                mercadorias.add(new Mercadoria(id, descricao, tipo, peso, volume, tagIoT));
+            }
+            readerMercadorias.close();
+
+            // Carregar transportadoras
+            BufferedReader readerTransportadoras = new BufferedReader(new FileReader("transportadoras.txt"));
+            while ((linha = readerTransportadoras.readLine()) != null) {
+                String[] dados = linha.split(",");
+                String id = dados[0];
+                String nome = dados[1];
+                double capacidadePeso = Double.parseDouble(dados[2]);
+                double capacidadeVolume = Double.parseDouble(dados[3]);
+                boolean especial = Boolean.parseBoolean(dados[4]);
+                transportadoras.add(new Transportadora(id, nome, capacidadePeso, capacidadeVolume, especial));
+            }
+            readerTransportadoras.close();
+
             System.out.println("Dados carregados com sucesso!");
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException e) {
             System.out.println("Erro ao carregar os dados: " + e.getMessage());
         }
     }
